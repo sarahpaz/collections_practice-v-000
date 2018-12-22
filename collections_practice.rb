812a608b455511c8de55d6c2eb72ccac63f8e896
Sarah Paz <sarah.paz@gmail.com>
2018-12-22 17:02:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,19 +1,3 @@
 def sort_array_asc(array)
   array.sort
 end
-
-def sort_array_desc(array)
-  array.sort.reverse
-end
-
-def sort_array_char_count(array)
-  array.sort_by {|word| word.length}
-end
-
-def swap_elements(array)
-  array[0], array[1], array[2] = array[0], array[2], array[1]
-end
-
-def reverse_array(array)
-  array.reverse
-end
