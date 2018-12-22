b29fc065aad08d13dff856b5d80cbf6db60cea24
Sarah Paz <sarah.paz@gmail.com>
2018-12-22 17:55:28 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|word| word.length}
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(integer)
+  integer.reverse
+end
+
+def kesha_maker(array)
+  array.each {|word| word[2] = "$"}
+end
+
+def find_a(array)
+  array.select {|word| word.start_with?('a')}
+end
+
+def sum_array(array)
+  array.inject {|sum, integer| sum + integer}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
