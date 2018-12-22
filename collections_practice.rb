9fe579180df46a720b5d61d4755355d46113a3b1
Sarah Paz <sarah.paz@gmail.com>
2018-12-22 17:44:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -31,5 +31,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  [1,2].each_with_index.collect{|element, index| [1] << "s" }
+  [1,2].each_with_index.collect{|element, index| element[1] << "s" }
 end
